c4a322757d3b676413f2c298983baf426d148ee6
andhm <andhm@126.com>
2016-01-31 15:16:59 +0800

    Update http_forwarder.c

--- http_forwarder.c
+++ http_forwarder.c
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Author: andhm@126.com                                                |
+  | Author: Min Huang <andhm@126.com>                                    |
   +----------------------------------------------------------------------+
 */
 
